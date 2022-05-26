f1f8ef398a1c251366daf7eda755496639646383
DrewPerkinsSoftRobot <drew.perkins@softrobot.io>
2022-05-26 23:09:02 +0200

    push build

---
+++ build/precache-manifest.67d368866770df86423edec61c2abd3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c9f6f97d864d06e3eafb54031626c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1b2c8409672ed4044c",
+    "url": "/static/css/main.4c7b676a.chunk.css"
+  },
+  {
+    "revision": "66cbf4251d4e0a171a6b",
+    "url": "/static/js/2.350b4efe.chunk.js"
+  },
+  {
+    "revision": "3c3833cc90e51ae822f01e296c687ebb",
+    "url": "/static/js/2.350b4efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1b2c8409672ed4044c",
+    "url": "/static/js/main.4f994055.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a78ffbbed2d858c61e068e3b756c9988",
+    "url": "/static/media/fa-brands-400.a78ffbbe.ttf"
+  },
+  {
+    "revision": "cd2b4095e9ce66cde642c3502a4022d9",
+    "url": "/static/media/fa-brands-400.cd2b4095.woff2"
+  },
+  {
+    "revision": "55b416a8df21f9f987aa352f10d1343b",
+    "url": "/static/media/fa-solid-900.55b416a8.woff2"
+  },
+  {
+    "revision": "738201559a50502aacabdbdb02720910",
+    "url": "/static/media/fa-solid-900.73820155.ttf"
+  }
+]);
